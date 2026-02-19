bf5d46a0bf8c489e973ca58fb70542be1563bac2
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2026-02-19 18:34:15 -0300

    Update aula1_operacoes_io.dart

--- aula1_operacoes_io/bin/aula1_operacoes_io.dart
+++ aula1_operacoes_io/bin/aula1_operacoes_io.dart
@@ -9,7 +9,7 @@ main() {
   //print('Nome: ' + nome + '. Idade: ' + idade.toString());
 
   //interpolação
-  print('Nome : $nome. Idade: $idade');
+  print('Nome : $nome. Idade: $idade. Nota: $nota. Matricula: $matriculado');
 
   //imprima o nome e a nota com um ponto extra como o ex abaixo
   //Nome do aluno: Ricardo. Nota final: 9.6
@@ -43,5 +43,28 @@ main() {
 
     print('v1: $v1');
     print('v2: $v2');
+
+  //ordem da operações é igual da matemática
+  //( ) indica prioridade 
+  double nota1 = 8;
+  double nota2 = 2;
+  print('Média = ${(nota1 + nota2) / 2}');
+
+  //dado um número de jogadores, imprimir o número de times 
+  // e o número de pessoas que ficarão sem time
+  // considere um time de 5 pessoas, sem jogador reserva
+  //ex nJog = 17
+  //times: 3
+  //fora: 2
+
+  int numJog = 23;
+  int times = (numJog/5).toInt();
+  int fora = (numJog%5).toInt();
+
+  print('Nº de Jogadores: $numJog.');
+  print('Nº de Times: $times.');
+  print('Nº de Jogadores sem time: $fora.');
+
+
 }
  
